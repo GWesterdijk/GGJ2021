696ea4831aa29432a2ea6e4d6391debe07085a7b
Jeffrey1509 <65658404+Jeffrey1509@users.noreply.github.com>
2021-01-29 18:59:36 +0100

    Tried but failed to make line of sight

---
+++ Global Game Jam 2021 Unity Project/Assets/Scripts/line_of_sight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class line_of_sight : MonoBehaviour
+{
+    private bool is_insight = false;
+    public Transform Player;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray sightRay = new Ray(transform.position, (transform.position-Player.position));
+
+       
+            if (Physics.Raycast(sightRay, out hit, LayerMask.NameToLayer("Default")))
+            {
+                if(hit.collider.tag == "Player")
+                {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                is_insight = true;
+                    Debug.Log("True");
+                }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            }
+            }
+        
+    }
+}
