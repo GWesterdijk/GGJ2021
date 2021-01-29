d8287a4aeae02d24e6ea69aa6ec64000ce8a1673
Gijs Westerdijk <gwesterdijk01@gmail.com>
2021-01-29 20:56:28 +0100

    Added waypoint system for enemy AI

--- Global Game Jam 2021 Unity Project/Assets/Scripts/AI/Human.cs
+++ Global Game Jam 2021 Unity Project/Assets/Scripts/AI/Human.cs
@@ -18,15 +18,87 @@ public class Human : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public enum State
     {
-        
+        Walking,
+        Searching,
+        Chasing
+    }
+    public State CurrentState = State.Walking;
+
+    float timer = 0;
+    [SerializeField] private RoomWaypoint currentRoom;
+
+    private void Start()
+    {
+        currentRoom = RoomWaypoint.Waypoints[Random.Range(0, RoomWaypoint.Waypoints.Count)];
+        WalkToNewRoom();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (CurrentState)
+        {
+            case State.Walking:
+                // Walk toward selected waypoint
+                if (NavMeshAgent.remainingDistance < NavMeshAgent.stoppingDistance)
+                {
+                    StartSearhingForCat();
+                }
+                break;
+            case State.Searching:
+                // Wait for certain time and find new waypoint
+                timer -= Time.deltaTime;
+
+                if (timer <= 0)
+                {
+                    WalkToNewRoom();
+                }
+                break;
+            case State.Chasing:
+                // Chase that darn cat
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Makes the human run to the room the cat is in
+    /// </summary>
+    public void TriggerHearingCat()
+    {
+
+    }
+
+    /// <summary>
+    /// Gets a new waitpoint and starts navigating to it
+    /// </summary>
+    public void WalkToNewRoom()
+    {
+        currentRoom = RoomWaypoint.Waypoints[Random.Range(0, RoomWaypoint.Waypoints.Count)];
+        NavMeshAgent.isStopped = false;
+        NavMeshAgent.SetDestination(currentRoom.transform.position);
+        CurrentState = State.Walking;
+    }
+
+    /// <summary>
+    /// Checks to see if cat is in line of sight and if so starts chasing
+    /// </summary>
+    public void StartSearhingForCat()
+    {
+        NavMeshAgent.isStopped = true;
+        // TODO: Play searching animation
+        timer = currentRoom.WaitTime;
+        CurrentState = State.Searching;
+    }
+
+    /// <summary>
+    /// Makes the human chase the cat
+    /// </summary>
+    public void StartChasingCat()
+    {
+
     }
 }
