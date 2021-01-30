99b6c930539d73b999e89a9855daeae1c19a8b02
Gijs Westerdijk <gwesterdijk01@gmail.com>
2021-01-30 09:38:42 +0100

    Added chaos script for thrwoing thing on the floor

---
+++ Global Game Jam 2021 Unity Project/Assets/Scripts/Chaos/Chaos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaos : MonoBehaviour
+{
+    protected bool chaosDone = false;
+    public int ChaosScore;
+
+    public virtual void DoChaos()
+    {
+        if (chaosDone)
+            return;
+        chaosDone = true;
+
+        // eat ass
+        // Add chaos counter to total chaos meter
+        Debug.Log("Did Chaos + Score: " + ChaosScore, transform);
+    }
+}
---
+++ Global Game Jam 2021 Unity Project/Assets/Scripts/Chaos/GroundFall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class GroundFall : Chaos
+{
+    [SerializeField] private List<string> collisionTags = new List<string>() { "Floor" };
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collisionTags.Contains(collision.transform.tag))
+        {
+            DoChaos();
+        }
+    }
+
+    public override void DoChaos()
+    {
+        if (chaosDone)
+            return;
+
+        base.DoChaos();
+
+        Debug.Log("Chaos After", transform);
+    }
+}
