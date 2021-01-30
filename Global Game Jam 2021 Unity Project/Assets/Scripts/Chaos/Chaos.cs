541b74ca54077dbb48ab8cc672e3abae7c9bcf0c
Gijs Westerdijk <gwesterdijk01@gmail.com>
2021-01-30 10:24:00 +0100

    Added UI that shows earned chaos points

--- Global Game Jam 2021 Unity Project/Assets/Scripts/Chaos/Chaos.cs
+++ Global Game Jam 2021 Unity Project/Assets/Scripts/Chaos/Chaos.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Chaos : MonoBehaviour
 {
+
     protected bool chaosDone = false;
+    public string ChaosTitle;
     public int ChaosScore;
+
 
     public virtual void DoChaos()
     {
@@ -15,6 +18,7 @@ public class Chaos : MonoBehaviour
 
         // eat ass
         // Add chaos counter to total chaos meter
+        ChaosUI.instance.ShowEarnedChaos(ChaosTitle, ChaosScore);
         Debug.Log("Did Chaos + Score: " + ChaosScore, transform);
     }
 }
