90cf7c26ba437862f9a3aa4942675662f42367ea
Gijs Westerdijk <gwesterdijk01@gmail.com>
2021-01-30 22:04:27 +0100

    Fixed camer clipping through player

--- Global Game Jam 2021 Unity Project/Assets/Scripts/PlayerCamera.cs
+++ Global Game Jam 2021 Unity Project/Assets/Scripts/PlayerCamera.cs
@@ -27,7 +27,7 @@ public class PlayerCamera : MonoBehaviour
         Vector3 targetPosition = target.position + Quaternion.Euler(rotationOffset) * offset;
 
         // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
+        int layerMask = LayerMask.GetMask("Player"); //1 << 8;
 
         // This would cast rays only against colliders in layer 8.
         // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
